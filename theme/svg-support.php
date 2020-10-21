2996e9106aa43d0a9cffc99128c04b7760b66500
A Dominy <11884699+gillespieza@users.noreply.github.com>
2020-10-21 12:13:24 +0100

    Update svg-support.php

--- theme/svg-support.php
+++ theme/svg-support.php
@@ -13,3 +13,22 @@ function apway_mime_types( $mimes ) {
 	return $mimes;
 }
 add_filter( 'upload_mimes', 'apway_mime_types' );
+
+
+
+/**
+ *
+ * `wp_check_filetype_and_ext` attemps to determine the real file type of a file.
+ * If unable to, the file name extension will be used to determine type.
+ */
+function apway_verify_filetype( $data, $file, $filename, $mimes ) {
+	$filetype = wp_check_filetype( $filename, $mimes );
+
+	return array(
+		'ext'             => $filetype['ext'],
+		'type'            => $filetype['type'],
+		'proper_filename' => $data['proper_filename'],
+	);
+
+}
+add_filter( 'wp_check_filetype_and_ext', 'apway_verify_filetype', 10, 4 );

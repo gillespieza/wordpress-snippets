96e87685df55ab6e54a83eb6b06ebf96ac051f4e
A Dominy <amanda.dominy@appiercom.com>
2021-04-08 11:48:03 +0100

    Improved commenting, added function to write to error log

--- debugger.php
+++ debugger.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Debugging functions
+ * Pretty-print various debugging functions.
  *
- * @package pathway_2020
+ * @package pathway-lms-connector\debugging
  */
 
 // Security Check: Prevent this file being executed outside the WordPress context.
@@ -12,13 +12,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! function_exists( 'print_r_hidden' ) ) {
 	/**
-	 * Wraps the print_r function in a hidden comment tag.
+	 * Wraps the print_r function in a comment tag so it's not visible except in the HTML source.
+	 *
+	 * Useful if you need to debug on a live site - you'll have to view the HTML
+	 * source to see the debugging. Also, you can search for FOOBAR or GREP or
+	 * whatever to find it.
 	 *
 	 * @param mixed $argument Whatever you want printed out.
 	 */
 	function print_r_hidden( $argument ) {
 		echo '<!-- FOOBAR GREP ';
-		print_r( $argument ); // phpcs:ignore
+		print_r( $argument ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r.
 		echo ' -->';
 	}
 }
@@ -32,7 +36,7 @@ if ( ! function_exists( 'print_r_pre' ) ) {
 	 */
 	function print_r_pre( $argument ) {
 		echo "<pre style='color: steelblue; background: #eee; border: 1px dashed #bbb; padding: 10px'>";
-		print_r( $argument );
+		print_r( $argument ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r.
 		echo '</pre>';
 	}
 }
@@ -43,7 +47,7 @@ if ( ! function_exists( 'print_filters_for' ) ) {
 	 *
 	 * For example `print_filters_for( 'the_content' );`
 	 *
-	 * @param string $hook Whatever you want printed out. Default is empty.
+	 * @param string $hook The hook that you are querying. Default is empty.
 	 */
 	function print_filters_for( $hook = '' ) {
 		global $wp_filter;
@@ -51,7 +55,27 @@ if ( ! function_exists( 'print_filters_for' ) ) {
 			return;
 		}
 		print '<pre>';
-		print_r( $wp_filter[ $hook ] );
+		print_r( $wp_filter[ $hook ] ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r.
 		print '</pre>';
+	}
+}
+
+
+
+if ( ! function_exists( 'write_log' ) ) {
+	/**
+	 * Write to the WordPress error log (usually found at `wp-content/debug.log`).
+	 *
+	 * @param  mixed $log The error message to print out.
+	 * @return void
+	 */
+	function write_log( $log ) {
+		if ( true === WP_DEBUG ) {
+			if ( is_array( $log ) || is_object( $log ) ) {
+					error_log( print_r( $log, true ) );
+			} else {
+					error_log( $log );
+			}
+		}
 	}
 }

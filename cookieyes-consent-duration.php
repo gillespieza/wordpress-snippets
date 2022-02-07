7090111166266ddea348870e51b700ac9bdf8d03
A Dominy <11884699+gillespieza@users.noreply.github.com>
2022-02-07 16:09:05 +0000

    Create cookieyes-consent-duration.php

---
+++ cookieyes-consent-duration.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Change the duration of consent for the CookieYes plugin.
+ *
+ * @link https://www.webtoffee.com/how-to-modify-duration-of-cookie-consent/
+ */
+
+function cli_set_expire() { 
+  
+  // First time visit and class exists.
+	if ( class_exists( 'Cookie_Law_Info_Public' ) ) {
+	?>
+	<script type="text/javascript">
+
+		CLI_ACCEPT_COOKIE_EXPIRE = 30;
+		
+	</script>
+	<?php
+	}
+}
+add_action( 'wp_footer', 'cli_set_expire', 20 );

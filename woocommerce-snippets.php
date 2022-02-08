fff94d89ab491e6b2b29b4d51c215d4e642df22c
A Dominy <11884699+gillespieza@users.noreply.github.com>
2022-02-08 09:34:51 +0000

    Create woocommerce-snippets.php

---
+++ woocommerce-snippets.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Utility function to check if WooCommerce is activated
+ */
+if ( ! function_exists( 'is_woocommerce_activated' ) ) {
+	function is_woocommerce_activated() {
+		if ( class_exists( 'woocommerce' ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}

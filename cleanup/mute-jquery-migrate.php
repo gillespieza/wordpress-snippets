ea729d13aee16fcdcf1457f5f15d8dba88148e9c
A Dominy <11884699+gillespieza@users.noreply.github.com>
2024-04-30 18:07:18 +0100

    Delete cleanup directory
    Create a stand-alone plugin for these. See the repo: salamander-cleanup

--- cleanup/mute-jquery-migrate.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function apway_mute_jquery_migrator() {
-	echo '<script>jQuery.migrateMute = true;</script>';
-}
-add_action( 'wp_head', 'apway_mute_jquery_migrator' );
-add_action( 'admin_head', 'apway_mute_jquery_migrator' );

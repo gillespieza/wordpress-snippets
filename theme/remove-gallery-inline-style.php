4ab1f083dde8444af51220a43d6e2b65b1563bde
A Dominy <11884699+gillespieza@users.noreply.github.com>
2024-04-30 18:11:54 +0100

    Delete theme/remove-gallery-inline-style.php
    Moved into standalone plugin at salamander-cleanup

--- theme/remove-gallery-inline-style.php
+++
@@ -1,5 +0,0 @@
-/*
- * Remove the inline CSS that the gallery shortcode outputs - this is invalid
- * HTML, and we've put that CSS in the stylesheet anyway.
- */
-add_filter( 'use_default_gallery_style', '__return_false' );
